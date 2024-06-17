f0d94be3d935fd76e963c17384050f08fd42f5e1
DavidRavier <ravier.dav@gmail.com>
2024-06-17 20:06:55 +0200

    installation de Material UI et début du design

--- my-app/src/app/components/TruckCard.js
+++ my-app/src/app/components/TruckCard.js
@@ -4,8 +4,35 @@ Faire une carte avec l'image, une étiquette collée en haut avec le prix puis s
 les caractéristiques du camion avec les deux faux boutons réserver et en savoir plus
 */
 
-import Image from "next/image"
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-export const card = {
-
+export default function MediaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="Camion benne simple cabine"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Camion benne simple cabine
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Réserver</Button>
+        <Button size="small">EN SAVOIR PLUS</Button>
+      </CardActions>
+    </Card>
+  );
 }
--- my-app/src/app/test/page.js
+++ my-app/src/app/test/page.js
@@ -2,23 +2,27 @@ import Image from "next/image";
 
 export default function Page() {
     return (
+        <div className="flex bg-blue-600">
+
         <Image
         src="/MPLoc_transparent.png"
         alt="MPLoc Logo"
-        className=""
-        width={100}
-        height={24}
+        className="mx-8"
+        width={140}
+        height={30}
         priority
-        />     
+        />  
+          <div className="">
+          <li>
+              <ul>Accueil</ul>
+              <ul>Location de matériel</ul>
+              <ul>Conseils</ul>
+              <ul>Galerie photos</ul>
+              <ul>Contact</ul>
+          </li>
+      </div>   
+        </div>
         );
 
-        <div className="flex">
-            <li>
-                <ul>Accueil</ul>
-                <ul>Location de matériel</ul>
-                <ul>Conseils</ul>
-                <ul>Galerie photos</ul>
-                <ul>Contact</ul>
-            </li>
-        </div>
+      
 }

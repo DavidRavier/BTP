f3875a5aceae7fcdd3bf7b24e73f98fd8054b6af
DavidRavier <ravier.dav@gmail.com>
2024-06-21 19:39:54 +0200

    mise en place du component Footer

--- my-app/src/app/components/footer.js
+++ my-app/src/app/components/footer.js
@@ -3,63 +3,55 @@ import Divider from "@mui/material/Divider";
 
 export default function Footer() {
     return (
-        <div className="grid-cols-4">
-            <Image
-            src="/MPLoc_transparent.png"
-            alt="Logo MPLoc"
-            width={200}
-            height={12}
-            />
-            <li>
-                <li>Informations</li>
-                    <ul>Accueil</ul>
-                    <ul>Nos agences</ul>
-                    <ul>Locations matériaux</ul>
-                    <ul>Contact</ul>
-                <li>Contact</li>
-                    <ul>Agence de Muret:</ul>
-                    <ul>Lorem</ul>
-                    <ul>Agence de Carères:</ul>
-                    <ul>Lorem</ul>
-                    <ul>Agence Autérive</ul>
-                    <ul>Lorem</ul>
-                    <ul>Tél: 05 34 51 57 91</ul>
-                    <ul>Mail: loremipsum@lorem.com</ul>
-                <li>Informations</li>
-            </li>
+        <div className="grid text-light bg-primary lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <Image
+          src="/MPLoc_transparent.png"
+          alt="Logo MPLoc"
+          className="mr-8 ml-4 my-8"
+          width={200}
+          height={12}
+          />
+            <h2 className={`my-8 text-xs py-2`}>
+              Informations
+              <Divider variant="middle" component="p" className="bg-light my-2"/>
+                <p>Accueil</p>
+                <p>Nos agences</p>
+                <p>Locations matériaux</p>
+                  <p>Gros matériel</p>
+                  <p>Petit matériel</p>
+                  <p>Matériel spécifique</p>
+                  <p>Matériel benne</p>
+                <p>Contact</p>
+            </h2>
+  
+            <h2 className={`my-8 text-xs py-2`}>
+              Contact{" "}
+              <Divider variant="middle" component="p" className="bg-light my-2"/>
+                <p>Agence de Muret:</p>
+                <p>Lorem</p>
+                <p>Agence de Carères:</p>
+                <p>Lorem</p>
+                <p>Agence Autérive</p>
+                <p>Lorem</p>
+                <p>Tél: 05 34 51 57 91</p>
+                <p>Mail: loremipsum@lorem.com</p>
+            </h2>
+  
+            <h2 className={`my-8 text-xs py-2`}>
+              Informations{" "}
+              <Divider variant="middle" component="p" className="bg-light mt-2"/>
+              <Image
+                src="/Logo_editions_corses.png"
+                alt="logo editions corses"
+                className="my-4"
+                width={150}
+                height={12}
+                priority
+              />
+              <p>© 2024 - Les éditions corses</p>
+              <p>Pexels</p>
+              <p>Mentions légales</p>
+            </h2>
         </div>
     )
 }
-
-/*
-
-<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-<h2 className={`mb-3 text-2xl font-semibold`}>
-Informations{" "}
-<li>
-<ul>Accueil</ul>
-<ul>Nos agences</ul>
-<ul>Locations matériaux</ul>
-<ul>Contact</ul>
-</li>
-</h2>
-
-<h2 className={`mb-3 text-2xl font-semibold`}>
-Contact{" "}
-<li>
-<ul>Agence de Muret:</ul>
-<ul>Lorem</ul>
-<ul>Agence de Carères:</ul>
-<ul>Lorem</ul>
-<ul>Agence d'Autérive</ul>
-<ul>Lorem</ul>
-<ul>Tél: 05 34 51 57 91</ul>
-<ul>Mail: loremipsum@lorem.com</ul>
-</li>
-</h2>
-
-<h2 className={`mb-3 text-2xl font-semibold`}>
-Informations{" "}
-</h2>
-</div>
-*/
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -198,57 +198,7 @@ export default function Home() {
         <p>Votre agence est ouverte du lundi au vendredi de 7h à 12h et de 13h45 à 18h.</p>
         <p> Nous vous accueillons également dans nos autres agences.</p>
       </div>
-
-      <div className="grid text-light bg-primary lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-      <Image
-        src="/MPLoc_transparent.png"
-        alt="Logo MPLoc"
-        className="mr-8 ml-4 my-8"
-        width={200}
-        height={12}
-        />
-          <h2 className={`my-8 text-xs py-2`}>
-            Informations
-            <Divider variant="middle" component="p" className="bg-light my-2"/>
-              <p>Accueil</p>
-              <p>Nos agences</p>
-              <p>Locations matériaux</p>
-                <p>Gros matériel</p>
-                <p>Petit matériel</p>
-                <p>Matériel spécifique</p>
-                <p>Matériel benne</p>
-              <p>Contact</p>
-          </h2>
-
-          <h2 className={`my-8 text-xs py-2`}>
-            Contact{" "}
-            <Divider variant="middle" component="p" className="bg-light my-2"/>
-              <p>Agence de Muret:</p>
-              <p>Lorem</p>
-              <p>Agence de Carères:</p>
-              <p>Lorem</p>
-              <p>Agence Autérive</p>
-              <p>Lorem</p>
-              <p>Tél: 05 34 51 57 91</p>
-              <p>Mail: loremipsum@lorem.com</p>
-          </h2>
-
-          <h2 className={`my-8 text-xs py-2`}>
-            Informations{" "}
-            <Divider variant="middle" component="p" className="bg-light mt-2"/>
-            <Image
-              src="/Logo_editions_corses.png"
-              alt="logo editions corses"
-              className="my-4"
-              width={150}
-              height={12}
-              priority
-            />
-            <p>© 2024 - Les éditions corses</p>
-            <p>Pexels</p>
-            <p>Mentions légales</p>
-          </h2>
-      </div>
+<Footer />
     </main>
   );
 }
